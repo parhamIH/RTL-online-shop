ee58ecedd1f4bd5fcd293953fa56c4f6d2790b0f
parha,IH <parham.ilaghi@gmail.com>
2025-07-22 23:43:04 +0330

    delpoyed version

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,9 +1,20 @@
+"""
+WSGI config for main project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
 
 import os
+import sys
+
+path = '/home/fatiimaa/RTL-online-shop'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-
 application = get_wsgi_application()
