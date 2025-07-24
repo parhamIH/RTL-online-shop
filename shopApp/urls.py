762d6e2c58b28f29c7da8a3ae9e6944180c11a78
parha,IH <parham.ilaghi@gmail.com>
2025-07-24 03:39:45 +0330

    updated

--- shopApp/urls.py
+++ shopApp/urls.py
@@ -7,7 +7,7 @@ from django.utils.text import slugify
 
 urlpatterns = [
     path("products", products_list),
-    path('product/<pk>/<name>', product_detail),
+    path('product/<pk>/<name>', product_detail, name='product_detail'),
     path ("categories/<en_name>", category_products),    
     path("get-package-info/<int:package_id>/", get_package_info, name='get_package_info'),
     path("page/<slug:slug>/", static_page, name='static_page'),
